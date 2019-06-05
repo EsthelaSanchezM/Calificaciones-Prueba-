df64b210f830c28a5961ef95791b3478c508aa4c
JuanRjs <51392343+JuanRjs@users.noreply.github.com>
2019-06-04 20:04:46 -0700

    Add files via upload

---
+++ Form/Subir-archivos.aspx.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calificaciones.Form
+{
+    public partial class Subir_archivos : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLoad_Click(object sender, EventArgs e)
+        {
+            string filename = Path.GetFileName(upData.FileName);
+           
+           upData.SaveAs(Server.MapPath("../Files/") + filename);
+            string route =Server.MapPath("../Files/") + filename;
+            // lblStatus .Text = "Archivo subido con éxito, ahora puede consultar su información.";
+            // Ruta = Server.MapPath("Archivos/") + filename;
+            // Extension = Path.GetExtension(upData.PostedFile.FileName);
+            //btnGenerarInformacion.Visible = true;
+
+            ShowData(route); 
+        }
+
+
+        private void ShowData(string routeguide )
+        {
+
+
+            string conStr = "";
+            conStr=ConfigurationManager.ConnectionStrings["Excel07ConString"]
+                          .ConnectionString;
+
+
+          
+            conStr = String.Format(conStr, routeguide, "Yes");
+            OleDbConnection connExcel = new OleDbConnection(conStr);
+            OleDbCommand cmdExcel = new OleDbCommand();
+            OleDbDataAdapter oda = new OleDbDataAdapter();
+            DataTable dtInformation = new DataTable();
+            cmdExcel.Connection = connExcel;
+
+            
+            
+                connExcel.Open();
+                DataTable dtExcelSchema;
+                dtExcelSchema = connExcel.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string SheetName = dtExcelSchema.Rows[0]["TABLE_NAME"].ToString();
+
+
+            cmdExcel.CommandText = "SELECT * FROM [" + SheetName + "] ";
+
+
+
+            oda.SelectCommand = cmdExcel;
+                oda.Fill(dtInformation);
+                connExcel.Close();
+
+            table.AutoGenerateColumns = false;
+                table.DataSource = dtInformation;
+
+            table.DataBind();
+            }
+        }
+    }
---
+++ Form/Subir-archivos.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Calificaciones.Form {
+    
+    
+    public partial class Subir_archivos {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control upData.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.FileUpload upData;
+        
+        /// <summary>
+        /// Control btnLoad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLoad;
+        
+        /// <summary>
+        /// Control table.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView table;
+        
+        /// <summary>
+        /// Control divMenciones.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divMenciones;
+    }
+}
